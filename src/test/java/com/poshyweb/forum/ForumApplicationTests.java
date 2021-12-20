4eafe2afe7f8b353ff6022962fe9f28b9a246ffd
Roberto Moraes <robertomoraes@lampp-it.com.br>
2021-12-20 17:20:27 -0500

    Primeiro commit do projeto

---
+++ src/test/java/com/poshyweb/forum/ForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poshyweb.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
